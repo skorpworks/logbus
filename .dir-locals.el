4ece3989036116477561d6ef85ab00e05ed97780
Erik Stephens <estephens@terascope.io>
2017-11-01 16:26:26 -0700

    Show my emacs friends some love

---
+++ .dir-locals.el
@@ -0,0 +1,8 @@
+((nil . ((eval . (progn
+                   (add-to-list 'exec-path (concat (locate-dominating-file default-directory ".dir-locals.el") "node_modules/.bin/"))
+                   (custom-set-variables
+                    '(c-basic-offset 2)
+                    '(js-indent-level 2)
+                    '(js2-basic-offset 2)
+                    )
+                   )))))
